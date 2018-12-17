a62d2505d8b94e8cf9729cd6151ab990d1356661
AhsanRiyad <riyad298@gmail.com>
2018-12-17 10:58:10 +0600

    stable

--- mvc/view/viewIndexCart.php
+++ mvc/view/viewIndexCart.php
@@ -59,6 +59,88 @@ $totalPrice = 0;
 	</table>
 	
 	<hr>
-	<a href="<?php echo $indexUrl; ?>" class="btn btn-warning text-white">Confirm</a>
+	<a  id="idButtonIndexPageConfirmOrder" href="<?php echo $indexUrl; ?>" class="btn btn-warning text-white">Confirm</a>
 	<a href="<?php echo $indexUrl; ?>" class="btn btn-info">Continue Shopping</a>
 </div>
+
+
+
+
+<?php 
+$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+
+$sArray = $_SESSION[$SessionCheckUserInfo];	
+$date = date("Y-m-d H:i:s");
+
+$email = $sArray['email'];
+$sql = "INSERT INTO `orderproduct`( `orderDate`, `orderStatus`, `userId`) VALUES ('$date','pending','$email')";
+
+$result = mysqli_query($conn, $sql);
+mysqli_close($conn);
+$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+$date = $date.'.000000';
+$sql = "SELECT `orderId`, `orderDate`, `orderStatus`, `userId` FROM `orderproduct` WHERE userId='$email' and orderDate='$date'";
+	//echo $sql;
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+mysqli_close($conn);
+$orderId = $row['orderId'];
+	echo $orderId; //var1
+
+
+
+
+
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+
+	$sql = "SELECT * FROM `cart` WHERE userId='$email'";
+	$result = mysqli_query($conn, $sql);
+	mysqli_close($conn);
+
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+
+	
+
+	while($row = mysqli_fetch_assoc($result)){
+
+		$productId=$row['productId'];
+		$quantity = $row['quantity'];
+		$sellerId = $row['sellerId'];
+		$email=$sArray['email'];
+
+
+		$sql = "INSERT INTO `contains`(`orderId`, `productId`, `perProductQuantity`, `sellerId`, `userId`) VALUES ($orderId,$productId,$quantity,'$sellerId','$email')";
+
+
+		mysqli_query($conn, $sql);
+
+
+
+		
+
+		
+	}
+
+
+
+
+
+
+
+	?>
+
+
+
+
+
+
+
+	<script>
+
+
+		jsProfileInfo = {   orderDate: '<?php echo $date ?>' ,  orderStatus: 'pending' , userId: '<?php echo $sArray['email'] ?>' };
+		jsonStringDbParam = JSON.stringify(jsProfileInfo);
+	</script>
+
+
+
