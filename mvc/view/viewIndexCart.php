761852617aba17570d5dd7a5121728cb8096d9fd
AhsanRiyad <riyad298@gmail.com>
2018-12-19 22:19:49 +0600

    fixed

--- mvc/view/viewIndexCart.php
+++ mvc/view/viewIndexCart.php
@@ -6,8 +6,63 @@ $result = mysqli_query($conn, $sql);
 mysqli_close($conn);
 $i = 0;
 $totalPrice = 0;
+?>
+
+<?php 
+
+if (isset($_POST['submit'])) {	
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+
+	$sArray = $_SESSION[$SessionCheckUserInfo];	
+	$date = date("Y-m-d H:i:s");
+
+	$email = $sArray['email'];
+	$sql = "INSERT INTO `orderproduct`( `orderDate`, `orderStatus`, `userId`) VALUES ('$date','pending','$email')";
+
+	$result = mysqli_query($conn, $sql);
+	mysqli_close($conn);
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+	$date = $date.'.000000';
+	$sql = "SELECT `orderId`, `orderDate`, `orderStatus`, `userId` FROM `orderproduct` WHERE userId='$email' and orderDate='$date'";
+	//echo $sql;
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	mysqli_close($conn);
+	$orderId = $row['orderId'];
+	echo $orderId; //var1
+
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+
+	$sql = "SELECT * FROM `cart` WHERE userId='$email'";
+	$result = mysqli_query($conn, $sql);
+	mysqli_close($conn);
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+	while($row = mysqli_fetch_assoc($result)){
+
+		$productId=$row['productId'];
+		$quantity = $row['quantity'];
+		$sellerId = $row['sellerId'];
+		$email=$sArray['email'];
+
+
+		$sql = "INSERT INTO `contains`(`orderId`, `productId`, `perProductQuantity`, `sellerId`, `userId`) VALUES ($orderId,$productId,$quantity,'$sellerId','$email')";
+
+
+		mysqli_query($conn, $sql);
+
+	}	
+	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
+	$sql = "DELETE FROM `cart` WHERE `userId`='$email'";
+	mysqli_query($conn, $sql);
+}
 
 ?>
+
+
+
+
+
+
 
 <div class="container">
 	
@@ -19,12 +74,6 @@ $totalPrice = 0;
 		</h3>";
 	}
 	?>
-
-
-
-	
-	
-
 	<table class="table">
 		<thead class="thead-dark">
 			<tr>
@@ -79,63 +128,3 @@ $totalPrice = 0;
 	</form>
 	<a href="<?php echo $indexUrl; ?>" class="btn btn-info">Continue Shopping</a>
 </div>
-
-<?php 
-
-if (isset($_POST['submit'])) {	
-	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
-
-	$sArray = $_SESSION[$SessionCheckUserInfo];	
-	$date = date("Y-m-d H:i:s");
-
-	$email = $sArray['email'];
-	$sql = "INSERT INTO `orderproduct`( `orderDate`, `orderStatus`, `userId`) VALUES ('$date','pending','$email')";
-
-	$result = mysqli_query($conn, $sql);
-	mysqli_close($conn);
-	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
-	$date = $date.'.000000';
-	$sql = "SELECT `orderId`, `orderDate`, `orderStatus`, `userId` FROM `orderproduct` WHERE userId='$email' and orderDate='$date'";
-	//echo $sql;
-	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_assoc($result);
-	mysqli_close($conn);
-	$orderId = $row['orderId'];
-	echo $orderId; //var1
-
-	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
-
-	$sql = "SELECT * FROM `cart` WHERE userId='$email'";
-	$result = mysqli_query($conn, $sql);
-	mysqli_close($conn);
-
-
-
-	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
-	
-
-
-	while($row = mysqli_fetch_assoc($result)){
-
-		$productId=$row['productId'];
-		$quantity = $row['quantity'];
-		$sellerId = $row['sellerId'];
-		$email=$sArray['email'];
-
-
-		$sql = "INSERT INTO `contains`(`orderId`, `productId`, `perProductQuantity`, `sellerId`, `userId`) VALUES ($orderId,$productId,$quantity,'$sellerId','$email')";
-
-
-		mysqli_query($conn, $sql);
-
-	}
-	
-	$conn = mysqli_connect($hostName, $userName, $password , $databaseName);
-
-	$sql = "DELETE FROM `cart` WHERE `userId`='$email'";
-
-	mysqli_query($conn, $sql);
-}
-
-?>
-
